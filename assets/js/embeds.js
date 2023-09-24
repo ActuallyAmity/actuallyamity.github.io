6c76b0454c3c59a2c0bc052ccf2ca91b195001af
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-09-24 08:50:05 -0400

    deps: flow v0.4.0 -> v0.5.0

--- assets/js/embeds.js
+++ assets/js/embeds.js
@@ -1,27 +1,16 @@
-class Twitch {
-  static embedStream(targetId = 'twitch-embed') {
-    const target =
-      targetId instanceof Element
-        ? targetId
-        : document.getElementById(targetId);
+FlowTheme.lazy('youtube-embed', 30, () => {
+  new FlowTheme.EmbedHelper.YouTube('UCQ9nP4I7Ql3o-9jmMPd2I2A', 'youtube-embed')
+    .fetchVideoData()
+    .then((data) => data.mostRecent.embed());
+});
 
-    !target && console.error(`[Twitch]: Element not found`);
+FlowTheme.lazy('twitch-embed', 30, (targetId = 'twitch-embed') => {
+  const target =
+    targetId instanceof Element ? targetId : document.getElementById(targetId);
 
-    const dataSrc = target.getAttribute('data-src');
+  !target && console.error(`[Twitch]: Element not found`);
 
-    // Append Site._currentURL to satify Twitch Embed requirement for a parent referrer
-    if (dataSrc) target.src = dataSrc + window.location.origin.split('//')[1];
-  }
-}
-
-const EMBED_ID = 'youtube-embed';
-const YouTube = new FlowTheme.EmbedHelper.YouTube(
-  'UCQ9nP4I7Ql3o-9jmMPd2I2A',
-  EMBED_ID
-);
-
-FlowTheme.lazy('twitch-embed', 30, Twitch.embedStream);
-
-YouTube.fetchVideoData().then((data) => {
-  FlowTheme.lazy(EMBED_ID, 30, data.mostRecent.embed());
+  const dataSrc = target.getAttribute('data-src');
+  // Append Site._currentURL to satify Twitch Embed requirement for a parent referrer
+  if (dataSrc) target.src = dataSrc + window.location.origin.split('//')[1];
 });
